804ef1260920c39b36bfc9900771bf7a85d18d20
Ammon Jeffery <savingphace@gmail.com>
2020-07-29 16:35:13 -0600

    finished testing engineer constructor

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -8,7 +8,7 @@ test("Can set GitHUb account via constructor", () => {
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("Foo", 1, testValue, "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
